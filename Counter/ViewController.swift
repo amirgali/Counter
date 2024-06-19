9f80b4c0ca7abca6b1bb996619f489a332aab57a
Amir <aturalinov@yandex.ru>
2024-06-19 12:49:20 +0500

    Add label and button

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var changeButton: UIButton!
+    var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = "Значение счётчика: \(count)"
     }
-
-
+    
+    @IBAction func buttonTouchUpInside(_ sender: Any) {
+        count += 1
+        countLabel.text = "Значение счётчика: \(count)"
+    }
 }
 
